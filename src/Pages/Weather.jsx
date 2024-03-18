6479148ce7c72c3b74061afbc2c418eda4ca8fd3
Moracus <hsharshsharma370@gmail.com>
2024-03-19 00:18:32 +0530

    added deployment

--- src/Pages/Weather.jsx
+++ src/Pages/Weather.jsx
@@ -9,7 +9,9 @@ const Weather = () => {
   const [search, setSearch] = useState("");
   const [loading, setLoading] = useState(false);
   const [weatherData, setWeatherData] = useState(null);
-  const [backgroundImage, setBackgroundImage] = useState("");
+  const [backgroundImage, setBackgroundImage] = useState(
+    "https://images.unsplash.com/photo-1592210454359-9043f067919b?q=80&w=1770&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+  );
   const fetchWeatherData = async (param = "Delhi") => {
     setLoading(true);
     try {
